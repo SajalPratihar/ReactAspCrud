311efd0391542c62a41ecd35e303839e9599f20e
SajalPratihar <pratiharsajal@gmail.com>
2024-02-26 21:21:11 +0530

    first commit

---
+++ ReactAspCrud/Models/StudentDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ReactAspCrud.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions <StudentDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-DRBU0E0;Initial Catalog=lbs;Integrated Security=True");
+        }
+
+    }
+}
